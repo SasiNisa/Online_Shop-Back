6867ef468bbedc93035aa56f34b6efa582679109
SasiNisa <127511379+SasiNisa@users.noreply.github.com>
2026-01-08 20:52:38 -0500

    Update to settings.py
    Update to settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -27,13 +27,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 #DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
 #ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
-"""
+
 ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1:8000",
-    "https://online-shop-back-algg.onrender.com/",
+    "online-shop-back-algg.onrender.com/",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
